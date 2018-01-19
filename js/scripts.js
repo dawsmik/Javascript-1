290c1edd306ca787604f2ae029b0540e7ef9569a
Mike Dawson <mikedawson@Mikes-MacBook-Pro.local>
2018-01-18 21:00:16 -0800

    primitives, objects etc

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+var sentence = 2;
+console.log(sentence);
+{
+  {
+    {
+
+    }
+  }
+}
+//Child inherits from parents, does not know kids.
+//purpose of variable is to point to something else.
+
+let firstNumber = 22; ///declaration and defenition
+var first; //declaration
+firstNumber = 22; //defenition
+var secondNumber = 11;
+
+//let doesnt hoist and only works locally (between brackets)
+
+{
+const truth = "Ep8 was.....TERRIBLE....";
+console.log(truth);
+}
+const truth = "ep8 was ok";
+
+
+
+console.log(truth);
+
+let rightNow = new Date();
+console.log(rightNow)
+
+var string = "lala";
+
+var newString = new String ("lala");
+
+console.log(string);
+console.log(newString.toUpperCase());
