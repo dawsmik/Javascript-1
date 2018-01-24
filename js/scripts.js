92671247300f1717dafe7fde374b8c9e4eeeb603
Mike Dawson <mikedawson@Mikes-MacBook-Pro.local>
2018-01-23 18:59:27 -0800

    day 2 stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,62 @@
-var sentence = 2;
-console.log(sentence);
-{
-  {
-    {
+// var sentence = 2;
+// console.log(sentence);
+// {
+//   {
+//     {
+//
+//     }
+//   }
+// }
+// //Child inherits from parents, does not know kids.
+// //purpose of variable is to point to something else.
+//
+// let firstNumber = 22; ///declaration and defenition
+// var first; //declaration
+// firstNumber = 22; //defenition
+// var secondNumber = 11;
+//
+// //let doesnt hoist and only works locally (between brackets)
+//
+// {
+// const truth = "Ep8 was.....TERRIBLE....";
+// console.log(truth);
+// }
+// const truth = "ep8 was ok";
+//
+//
+//
+// console.log(truth);
+//
+// let rightNow = new Date();
+// console.log(rightNow)
+//
+// var string = "lala";
+//
+// var newString = new String ("lala");
+//
+// console.log(string);
+// console.log(newString.toUpperCase());
+//
+// var myArray = ["Hello World",22, true, {},null];
+//
+//
+// var myObject = {
+//
+//   myString: "Hello World"
+//   magicNumber: 42
+//
+// };
+//
+// let helloMessage = alert("Hey, there! My book is only $1....");
+//
+//
+// let promptMessage = prompt("Put in Password....", "Password")
+// console.log (promptMessage);
 
-    }
-  }
-}
-//Child inherits from parents, does not know kids.
-//purpose of variable is to point to something else.
+var firstName = prompt("first name:");
+var lastName= prompt("last name:");
+console.log(firstName);
+alert(lastName);
 
-let firstNumber = 22; ///declaration and defenition
-var first; //declaration
-firstNumber = 22; //defenition
-var secondNumber = 11;
-
-//let doesnt hoist and only works locally (between brackets)
-
-{
-const truth = "Ep8 was.....TERRIBLE....";
-console.log(truth);
-}
-const truth = "ep8 was ok";
-
-
-
-console.log(truth);
-
-let rightNow = new Date();
-console.log(rightNow)
-
-var string = "lala";
-
-var newString = new String ("lala");
-
-console.log(string);
-console.log(newString.toUpperCase());
+let birthday = prompt("what's your Birthday?","Answer in folowing format: 01/01/1900");
+confirm(new Date(birthday));
